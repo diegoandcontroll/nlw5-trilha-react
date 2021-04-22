cbbb30c6878310851fb12f6025624662da6d702e
diegoandcontroll <lukasxdp2@gmail.com>
2021-04-22 11:33:01 -0300

    adicionando audio e utlizando context api useEffect e userRef

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import {GetStaticProps} from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { PlayerContext } from '../contexts/PlayerContext';
 import {format, lastDayOfDecade, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurantionToTimeString';
 import styles from './home.module.scss';
+
 
 type Episode ={
   id: string;
@@ -23,6 +26,7 @@ type HomeProps = {
   allEpisodes: Episode[];
 }
 export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
+  const {play} = useContext(PlayerContext);
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -47,7 +51,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episodio"/>
                 </button>
               </li>
